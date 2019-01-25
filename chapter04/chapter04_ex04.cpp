d42991cdfe1bb0fbf3d3d351c82fb2c54f081714
Aurme <46881096+Aurme@users.noreply.github.com>
2019-01-25 11:28:04 -0500

    Math expression
    I propose (what I suspect) to be a more mathematical way to express the idea that we are trying to do. I believe this shows what we intend to happen better than hardcoding the number halves. Still not quite modular, but I believe it helps get people closer to thinking in a modular way with this version.

--- chapter04/chapter04_ex04.cpp
+++ chapter04/chapter04_ex04.cpp
@@ -1,68 +1,55 @@
 // Chapter 04, exercise 04: guess a number from 1 to 100 in no more than 7 tries
 
-#include "../lib_files/std_lib_facilities.h"
-int main()
-{
-    cout << "Think of a number between 1 and 100.\n";
-    cout << "For every guess, enter if your number is\n";
-    cout << "(l)arger or (s)maller than the one shown.\n";
-    cout << "Enter 'c' (for 'correct') if the number shown is yours.\n";
+//The middle of a given range of numbers can be taken by adding the lowest number and highest number
+//and dividing it by two. we can set this to an expression of (max+min) / 2
+//since we remove our guess from the range, you can update these by adding 1 to the lowest possible
+//range if our guess was too low, or subtracting 1 if our guess was too high. (to explain the
+// else if()... bodies)
 
-    int guess = 50;
-    vector<int> differences(6);
-    differences[0] = 25;
-    differences[1] = 13;
-    differences[2] = 6;
-    differences[3] = 3;
-    differences[4] = 2;
-    differences[5] = 1;
-    int counter = 0;
-    char answer = ' ';
+//we rely on integer division for this problem, as a special note
+#include "std_lib_facilities.h"
 
-    while (answer != 'c') {
-        cout << "My guess: " << guess << endl;
-        ++counter;
-        cout << "Correct, larger or smaller (c, l, s)? ";
-        cin >> answer;
-        switch (answer) {
-        case 'c':
-            // do nothing, simply fall out of the loop
-            break;
-        case 'l':
-            // increase by next difference or one
-            if (counter<=differences.size())
-                guess += differences[counter-1];
-            else
-                ++guess;
-            break;
-        case 's':
-            // decrease by next difference
-            if (counter<=differences.size())
-                guess -= differences[counter-1];
-            else
-                --guess;
-            break;
-        default:
-            // invalid answer, reduce counter because guess does not count
-            cout << "This is not a valid answer, use 'c', 'l' or 's'\n";
-            --counter;
+int main() {
+    
+    int min_num = 1;
+    int max_num = 100;
+    int count = 1;
+    cout << "I will guess your number (1-100) in seven guesses or less. You just need to tell me " <<
+    "if my guess is (s)maller, (l)arger, or (c)orrect.\n\n";
+    cout << "guess " << count << ":\n";
+    cout << "Is your number " << (max_num + min_num) / 2 << "?\n";
+    char user_reply;
+    while(cin >> user_reply )
+    {
+        if(user_reply == 'c'){
+            
+            cout << "Thanks for playing.\n";
             break;
         }
-        if (guess>100 || guess<1) {
-            cout << "Your number must be between 1 and 100!\n";
-            return 0;
-        }
-        if (counter==7 && answer!='c')
+        
+        else if(user_reply == 'l')
         {
-            cout << "Super fishy, dude. I should have your number by now.\n";
-            return 0;
+            min_num = ((max_num + min_num) / 2) + 1;
         }
+        
+        else if(user_reply == 's')
+        {
+            max_num = ((max_num + min_num) / 2) - 1;
+        }
+        
+        else{
+            cout << "That wasn't a valid response. Please use (s)maller, (l)arger, or (c)orrect.\n";
+            count--; //reduce counter since invalid
+        }
+        count++;
+        if(count > 7){
+            cout << "That's odd, are you sure you picked a number within range? I used all my guesses.\n";
+            break;
+        }
+        cout << "guess " << count << ":\n";
+        cout << "Is your number " << (max_num + min_num) / 2 << "?\n";
+        
     }
-
-    string s_try;
-    if (counter==1)
-        s_try = " try ";
-    else
-        s_try = " tries ";
-    cout << "It took me " << counter << s_try << "to find your number, " << guess << ".\n";
+    
+     return 0;
 }
